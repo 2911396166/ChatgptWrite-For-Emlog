a73bf7ac9df866500709f1910c583e6b70b2bc83
fish9 <93854134+2911396166@users.noreply.github.com>
2023-02-24 18:03:20 +0800

    1.2版本上传
    修复一些小问题
    新增余额显示

--- chatgptApi.php
+++ chatgptApi.php
@@ -1,9 +1,5 @@
 <?php
 
- /**
-  * 本插件作者为吃猫的鱼
-  * 为开源插件，未经许可，不允许任何个体/企业对其进行二次开发/售卖！
-  */
 // 判断调用来路
 /* 允许跨域请求，接口建议允许 */
 header('Access-Control-Allow-Origin:*');
@@ -33,134 +29,85 @@ function str_re($str){
   return $str;
 }
 if($id==1){
-session_start();
-if (!$_SESSION['chatgptSessionPrompt']) {
-        $_SESSION['chatgptSessionPrompt'] = '';
-}
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-$prompt=$_SESSION['chatgptSessionPrompt'] .'\n提问:' . $text . '\n Ai:';
-$chatgptaray=array('\n提问:','\nAI:');
-curl_setopt($ch, CURLOPT_URL, "https://api.openai.com/v1/completions");
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_POSTFIELDS, '{
-  "model": "text-davinci-003",
-  "prompt": "'.$prompt.'",
-  "max_tokens": 2048
-}');
-curl_setopt($ch, CURLOPT_POST, 1);
-// Set the API key as an HTTP header
-$headers = array();
-$headers[] = "Content-Type: application/json";
-$headers[] = "Authorization: Bearer ".$key;
-curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-// Send the request and parse the response
-$response = curl_exec($ch);
-$response_data = json_decode($response, true);
-$_SESSION['chatgptSessionPrompt'] = $prompt . $response_data['data'];
-// echo($response);
-if (curl_errno($ch)) {
-  // If there was an error executing the cURL request, print it out
-  echo 'Error: ' . curl_error($ch);
-} else {
+  session_start();
+  if (!$_SESSION['chatgptSessionPrompt']) {
+          $_SESSION['chatgptSessionPrompt'] = '';
+  }
+  $ch = curl_init();
+  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+  curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+  $prompt=$_SESSION['chatgptSessionPrompt'] .'\n提问:' . $text . '\n Ai:';
+  $chatgptaray=array('\n提问:','\nAI:');
+  curl_setopt($ch, CURLOPT_URL, "https://api.openai.com/v1/completions");
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+  curl_setopt($ch, CURLOPT_POSTFIELDS, '{
+    "model": "text-davinci-003",
+    "prompt": "'.$prompt.'",
+    "max_tokens": 2048
+  }');
+  curl_setopt($ch, CURLOPT_POST, 1);
+  $headers = array();
+  $headers[] = "Content-Type: application/json";
+  $headers[] = "Authorization: Bearer ".$key;
+  curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+  $response = curl_exec($ch);
+  $response_data = json_decode($response, true);
+  $_SESSION['chatgptSessionPrompt'] = $prompt . (isset($response_data['data']) ? $response_data['data'] : "");
+  if (curl_errno($ch)) {
+    echo 'Error: ' . curl_error($ch);
+  } else {
 
-  $result = array(
-      'code'=> 200,
-      'msg'=>"获取成功",
-      'data'=>array(
-          'html'=> $response_data['choices'][0]['text'],
-          'title'=>$text
-      ),
-  );
-  echo json_encode($result,320);
-  exit();
-  
-}
-curl_close($ch);
-}
-if($id==2){
+    $result = array(
+        'code'=> 200,
+        'msg'=>"获取成功",
+        'data'=>array(
+            'html'=> (isset($response_data['choices'][0]['text']) ? $response_data['choices'][0]['text'] : ""),
+            'title'=>$text
+        ),
+    );
+    echo json_encode($result,320);
+    exit();
     
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, "https://api.openai.com/v1/images/generations");
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_POSTFIELDS, '{
-  "n": 1,
-  "prompt": "'.$text.'",
-  "size": "256x256"
-}');
-curl_setopt($ch, CURLOPT_POST, 1);
- 
-// Set the API key as an HTTP header
-$headers = array();
-$headers[] = "Content-Type: application/json";
-$headers[] = "Authorization: Bearer ".$key;
-curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-
-// Send the request and parse the response
-$response = curl_exec($ch);
-$response_data = json_decode($response, true);
-
-if (curl_errno($ch)) {
-  // If there was an error executing the cURL request, print it out
-  echo 'Error: ' . curl_error($ch);
-} else {
-
-  $result = array(
-      'code'=> 200,
-      'msg'=>"获取成功",
-      'data'=>array(
-          'url'=> $response_data['data'][0]['url'],
-          'title'=>$text
-      ),
-  );
-  echo json_encode($result,320);
-  exit();
-  
+  }
+  curl_close($ch);
 }
-curl_close($ch);
-}
+
+
 if($id==3){
-session_start();
-if (!$_SESSION['chatgptSessionPrompt']) {
-        $_SESSION['chatgptSessionPrompt'] = '';
-}
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-curl_setopt($ch, CURLOPT_URL, "https://api.openai.com/dashboard/billing/credit_grants");
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+  session_start();
+  if (!$_SESSION['chatgptSessionPrompt']) {
+          $_SESSION['chatgptSessionPrompt'] = '';
+  }
+  $ch = curl_init();
+  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+  curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+  curl_setopt($ch, CURLOPT_URL, "https://api.openai.com/dashboard/billing/credit_grants");
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 
- 
-// Set the API key as an HTTP header
-$headers = array();
-$headers[] = "Content-Type: application/json";
-$headers[] = "Authorization: Bearer ".$key;
-curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-
-// Send the request and parse the response
-$response = curl_exec($ch);
-$response_data = json_decode($response, true);
-$_SESSION['chatgptSessionPrompt'] = $prompt . $response_data['id'];
-// echo($response);
-if (curl_errno($ch)) {
-  // If there was an error executing the cURL request, print it out
-  echo 'Error: ' . curl_error($ch);
-} else {
-
-  $result = array(
-      'code'=> 200,
-      'msg'=>"获取成功",
-      'data'=>array(
-          'html'=> $response_data['total_available'],
-      ),
-  );
-  echo json_encode($result,320);
-  exit();
   
-}
-curl_close($ch);   
+  // Set the API key as an HTTP header
+  $headers = array();
+  $headers[] = "Content-Type: application/json";
+  $headers[] = "Authorization: Bearer ".$key;
+  curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+  $response = curl_exec($ch);
+  $response_data = json_decode($response, true);
+  $_SESSION['chatgptSessionPrompt'] = $prompt . (isset($response_data['id']) ? $response_data['id'] : "");
+  if (curl_errno($ch)) {
+    echo 'Error: ' . curl_error($ch);
+  } else {
+    $result = array(
+        'code'=> 200,
+        'msg'=>"获取成功",
+        'data'=>array(
+            'html'=>  (isset($response_data['total_available']) ? $response_data['total_available'] : ""),
+        ),
+    );
+    echo json_encode($result,320);
+    exit();
+    
+  }
+  curl_close($ch);   
 }
 ?>
--- chatgpt_write.php
+++ chatgpt_write.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: ChatgptWrite
-Version: 1.0
+Version: 1.1
 Plugin URL:https://www.fish9.cn/archives/387/
 Description: ChatgptWrite 是一个方便你在typecho使用chatgpt进行文章创作的插件！
 Author: 吃猫的鱼
@@ -28,7 +28,6 @@ function chatgptwrite() {
             }
             loading = 1;
             var str = document.getElementById("chatgptQuestion").value;
-            console.log(str)
                 axios.get('<?php echo $url_api; ?>', {
                     params: {
                         id: '1',
@@ -52,46 +51,68 @@ function chatgptwrite() {
                         loading = 0;
                     });
         }
-
+        function balance() {
+                        axios.get('<?php echo $url_api; ?>', {
+                            params: {
+                                id: '3',
+                                apikey: '<?php echo $plugin_storage->getValue('accessKey'); ?>'
+                            }
+                        })
+                            .then(function (response) {
+                                console.log(response["data"]["data"]["html"]);
+                                if(response["data"]["data"]["html"]==null){
+                                    document.getElementById("ye").innerHTML="出现错误，请检查您的AccessToken";
+                                }else{
+                                    document.getElementById("ye").innerHTML="当前余额："+response["data"]["data"]["html"];
+                                }
+                            })
+                            .catch(function (error) {
+                                document.getElementById("respone-div").innerHtml= error;
+                            })
+                            .then(function () {
+                            });
+        }
         function showChatgptAsk(){
             if(document.getElementById("ChatgptAsk").style.display=="none"){
                 document.getElementById("ChatgptAsk").style.display="block"; 
                 document.getElementById("respone-div").style.display="block"; 
+                document.getElementById("ye").style.display="block"; 
                 document.getElementById("chatgpt-button").innerHTML="收起chatgpt"; 
             }else{
                 document.getElementById("ChatgptAsk").style.display="none"; 
-                document.getElementById("respone-div").style.display="none"; 
+                document.getElementById("respone-div").style.display="none";
+                document.getElementById("ye").style.display="none"; 
                 document.getElementById("chatgpt-button").innerHTML="展开chatgpt"; 
             }
          } 
 
-
+        balance();
         function copy() {
-    const range = document.createRange();
-    range.selectNode(document.getElementById('respone'));
-    const selection = window.getSelection();
-    if(selection.rangeCount > 0) selection.removeAllRanges();
-    selection.addRange(range);
-    document.execCommand('copy');
-    }
-    document.getElementById('copy').addEventListener('click', copyArticle, false);
+        const range = document.createRange();
+        range.selectNode(document.getElementById('respone'));
+        const selection = window.getSelection();
+        if(selection.rangeCount > 0) selection.removeAllRanges();
+        selection.addRange(range);
+        document.execCommand('copy');
+        }
+        document.getElementById('copy').addEventListener('click', copyArticle, false);
 		
 	
 
         </script>   
 
         <div style="margin:5px 0px 5px 0px;text-align:left;">
-       <a class="btn btn-sm btn-primary" id="chatgpt-button" style = "text-decoration:none; color:white; padding:7px; margin:5px 0px 5px 0px"onclick="showChatgptAsk()">展开Chatgpt</a>
-        </div>
+       <a class="btn btn-sm btn-primary" id="chatgpt-button" style = "display:inline-block;text-decoration:none; color:white; padding:7px; margin:5px 0px 5px 0px"onclick="showChatgptAsk()">展开Chatgpt</a>
+        <div id="ye" style="display:none;"></div>    
+    </div>
        <div id ="ChatgptAsk" style="display:none;">
           <div >
-             <input type="text" placeholder="请输入询问内容..." class="w-70 text title" name="name" id="chatgptQuestion" value=""/><label onclick="chatgpt()" class="btn btn-sm btn-primary" style = "text-decoration:none; color:white; padding:7px 15px 7px 15px; margin:5px 5px 5px 5px">发送</label><label onclick="copy()" class="btn btn-sm btn-primary" style = "text-decoration:none; color:white; padding:7px 15px 7px 15px; margin:5px 5px 5px 5px">复制</label>
+             <input type="text" placeholder="请输入询问内容..." style="width:50%;display:inline-block;margin:6px 6px 6px 0px;"class="form-control" name="name" id="chatgptQuestion" value=""/><label onclick="chatgpt()" class="btn btn-sm btn-primary" style = "text-decoration:none; color:white; padding:7px 15px 7px 15px; margin:0px 5px 5px 5px">发送</label><label onclick="copy()" class="btn btn-sm btn-primary" style = "text-decoration:none; color:white; padding:7px 15px 7px 15px; margin:0px 5px 5px 5px">复制</label>
           </div>   
         </div>
         
          <div id="respone-div" style="display:none;">
-        <textarea  autocomplete="off" id="respone" name="respone" class="w-100 mono" rows="5" value="">
-        </textarea>
+        <textarea  autocomplete="off" id="respone" placeholder name="respone" class="form-control" rows="5" value=""></textarea>
         </div>
 
 <?php }
